ff06a2b0f3c241185f66b9b773640a0ebe1534a3
Sumancheera <sumancheera777@gmail.com>
2025-05-07 22:12:04 +0530

    queue using Deque

--- DSA/Queue_collections.py
+++ DSA/Queue_collections.py
@@ -1,20 +1,20 @@
 #from collections module we are importing  deque class(as function.)
-# from collections import deque
-# Deque1 = deque()
-# Deque1.append("a")
-# Deque1.append(1)
-# Deque1.append("b")
-# Deque1.append(2)
-# print("Initial queue")
-# print(Deque1)
-# print("\nElements dequeued from the queue")
-# print(Deque1.popleft()) # removes the first element from the deque
-# #print(Deque1.popleft())
-# print(Deque1.popleft()) # removes the next first element from the deque
-# print(Deque1.popleft())
-# print(Deque1.popleft())
-# print("\nQueue after removing elements") # prints the remaining elements in the deque
-# print(Deque1)
+from collections import deque
+Deque1 = deque()
+Deque1.append("a")
+Deque1.append(1)
+Deque1.append("b")
+Deque1.append(2)
+print("Initial queue")
+print(Deque1)
+print("\nElements dequeued from the queue")
+print(Deque1.popleft()) # removes the first element from the deque
+#print(Deque1.popleft())
+print(Deque1.popleft()) # removes the next first element from the deque
+print(Deque1.popleft())
+print(Deque1.popleft())
+print("\nQueue after removing elements") # prints the remaining elements in the deque
+print(Deque1)
 
 #using list, queue
 l=[]
